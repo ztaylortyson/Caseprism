e14a8087accd814aff323689391f6ec03b9068e9
ztaylortyson <ztaylortyson@yahoo.com>
2024-07-11 14:08:06 -0700

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :attorneys
   get 'msword', to: 'pages#msword'
   root 'pages#home'
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
